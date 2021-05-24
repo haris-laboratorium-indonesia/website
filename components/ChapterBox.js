28f39c20a97c198dc8515775322fadc1244e07ed
Harits Syah <haritssr@gmail.com>
2021-05-24 14:14:11 +0700

    a

--- components/ChapterBox.js
+++ components/ChapterBox.js
@@ -7,7 +7,7 @@ export default function ChapterBox(props) {
     <div className="group hover:bg-gray-100  flex items-center hover:rounded p-2 sm:py-1">
       <HiFolder className=" w-7 h-7 text-gray-800 mr-1" />
       <Link href={`${props.to}`}>
-        <a className="w-full truncate hover:underline hover:text-blue-500">{props.title}</a>
+        <a className="w-full truncate hover:underline hover:text-blue-600">{props.title}</a>
       </Link>
       <HiOutlineChevronRight className="w-5 h-5 text-gray-500  sm:hidden" />
     </div>
