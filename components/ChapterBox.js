3021f299babe2fdc9a35e33e33a0a44d13c7cfde
Harits Syah <haritssr@gmail.com>
2021-05-24 14:17:11 +0700

    a

--- components/ChapterBox.js
+++ components/ChapterBox.js
@@ -4,10 +4,10 @@ import { HiOutlineChevronRight } from "react-icons/hi";
 
 export default function ChapterBox(props) {
   return (
-    <div className="group hover:bg-gray-100  flex items-center hover:rounded p-2 sm:py-1">
+    <div className="group hover:bg-gray-200  flex items-center hover:rounded p-2 sm:py-1">
       <HiFolder className=" w-7 h-7 text-gray-800 mr-1" />
       <Link href={`${props.to}`}>
-        <a className="w-full truncate hover:text-blue-600">{props.title}</a>
+        <a className="w-full truncate ">{props.title}</a>
       </Link>
       <HiOutlineChevronRight className="w-5 h-5 text-gray-500  sm:hidden" />
     </div>
