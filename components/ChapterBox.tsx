c2d60141bc1687cd06664e6e15a72ef8afd80688
Harits Syah <haritssr@gmail.com>
2021-06-28 09:37:37 +0700

    bismillah 2

--- components/ChapterBox.tsx
+++ components/ChapterBox.tsx
@@ -5,12 +5,12 @@ const ChapterBox = ({ to, title }) => {
     <>
       <Link href={to}>
         <a className="flex items-center justify-between w-full px-2 py-2.5 sm:px-1.5 sm:py-1.5 sm:hover:bg-gray-200 sm:hover:rounded-md ">
-          <div className="flex items-center sm:justify-between space-x-2 sm:space-x-1">
-            <Folder />
+          <div className="flex items-center space-x-2 sm:justify-between sm:space-x-1">
+            <Folder color=" " />
             <p className="text-gray-700 truncate">{title}</p>
           </div>
           <div className="sm:hidden">
-            <RightArrow />
+            <RightArrow color=" " />
           </div>
         </a>
       </Link>
